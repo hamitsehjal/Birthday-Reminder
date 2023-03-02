20f190afc9565d7d5ca4eb8d585448f5691774fb
hamitsehjal <hamitsehjal26@gmail.com>
2023-03-02 18:30:32 -0500

    configured Upcomg Birthdays

--- components/UpcomingBirthday.jsx
+++ components/UpcomingBirthday.jsx
@@ -1,36 +1,35 @@
 import Image from "next/image";
 
-export default function Birthday({props}){
 
-
+export default function UpcomingBirthday({props}){
     const {name,image,birthday}=props;
-    console.log(image)
    return(<>
-   <div className=" max-w-sm mx-auto my-5 bg-white rounded-xl shadow-lg flex items-center space-x-4">
-    <div className="pl-2 shrink-0 shadow-lg">
-   {/* <img className="h-12 w-32" src="../public/barun.jpg" alt="{name}"/> */}
-   <Image width={100} height={100} src={image} alt={name}/>
+   <div className=" p-2 max-w-sm mx-auto my-5 bg-white rounded-xl shadow-lg flex items-center space-x-4">
+    <div className="pl-2 shrink-0 shadow-lg rounded-3xl">
+   <Image className="rounded-full" width={100} height={100} src={image} alt={name}/>
    </div>
     <div className="flex-col p-6">
         <h1>{getAge(birthday)}</h1>
         <p>{name}</p>
-        <p>{birthday}</p>
     </div>
    </div>
    </>)
-
-
 }
 
 const getAge=(dateString)=>{
     const today=new Date();
     const currentDay=Math.round(today.getUTCDay()+today.getTimezoneOffset()/60/24);
     const currentMonth=today.getUTCMonth();
+    const currentYear=today.getUTCFullYear();
 
     const date=new Date(dateString);
     const day=Math.round(date.getUTCDate()+date.getTimezoneOffset()/60/24);
     const month=date.getUTCMonth();
+    const year=date.getUTCFullYear();
 
-   return day;
+    const age=currentYear-year;
+
+
+   return age;
 
 }
--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,27 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import info from '@/public/data.jsx'
-import Birthday from '@/components/Birthday'
+import Birthday from '@/components/UpcomingBirthday'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   console.log(info)
   return (
     <>
-      {info.map((person, index) => (<Birthday props={person} />))}
-      {/* <Birthday props={info[0]} /> */}
+      <div className='flex'>
+        <div className='w-1/2'>
+          <h1 className="mt-4 text-3xl font-bold text-gray-800 mb-4 text-center">Today's Occasions</h1>
 
+          {info.map((person, index) => (<Birthday props={person} />))}
 
+        </div>
+        <div className='w-1/2'>
+          <h1 className="mt-4  text-3xl font-bold text-gray-800 mb-4 text-center">Upcoming Birthday</h1>
+          {info.map((person, index) => (<Birthday props={person} />))}
 
+        </div>
+      </div>
+      {/* Calling Birthday component on each person */}
 
 
     </>
